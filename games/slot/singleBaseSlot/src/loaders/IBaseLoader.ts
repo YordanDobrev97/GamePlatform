e64bf2a58e46b5e24c446c5d2b07db05d532ec8b
YordanDobrev <yordan.19Dobrev@gmail.com>
2024-11-22 23:06:18 +0200

    remove old structure with pixi 4 and upgrade pixi to latest version

--- games/slot/singleBaseSlot/src/loaders/IBaseLoader.ts
+++
@@ -1,7 +0,0 @@
-import { ILoader } from './ILoader'
-
-export interface IBaseLoader {
-    registerLoader(name: string, loader: ILoader) : void
-    getLoader(name: string): ILoader | undefined
-    loadAll(): Promise<void>
-}
--- games/slot/singleBaseSlot/src/loaders/ILoader.ts
+++
@@ -1,6 +0,0 @@
-import * as PIXI from 'pixi.js'
-
-export interface ILoader {
-    load(): Promise<void>
-    getResource(path: string): PIXI.loaders.Loader | undefined
-}
